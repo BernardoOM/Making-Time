630918e98e531986b646b92f77b12fe1d6d64227
Michael7825 <michaelkang@MacBook-Pro.local>
2016-02-10 01:56:06 -0800

    Click_Stats_Page

---
+++ XI_UNITY/Assets/Scripts/Stats_Page.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Stats_Page : MonoBehaviour {
+	// Use this for initialization
+	bool click = true;
+
+	public void onClick() {
+		if ( click)
+		{
+			transform.position = new Vector3 (1334 / 2f, 750 / 2f, 0);
+			click =false;
+			Debug.Log (click);
+		}
+		else if (!click)
+		{
+			transform.position = new Vector3 (-1334 / 2f, -750 / 2f, 0);
+			click =true;
+		}
+	}
+		
+
+}
